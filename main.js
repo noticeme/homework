87397dbb7115e649e068e1cca9c64b8d30dcdc88
noticeme <62439872+noticeme@users.noreply.github.com>
2020-11-27 17:38:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-let message = 'login';
-let message = 'Password';
+// var a = +prompt("1", '');
+// var b = +prompt("2", '');
+// var c = a + b;
+// alert(c);
 
-alert(message);
+
+
+
+
+// let a = +prompt("1", '');
+// let b = +prompt("2", '');
+// let c = a + b;
+// alert(c);
+
+
+function doSomething() {
+    let input = 'ssss';
+    input = prompt('Do something?');
+    if (input === null) {
+        return; //break out of the function early
