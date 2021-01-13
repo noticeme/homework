ecf6b71fb9a207f5061d3b89a8621bb2d7be6703
noticeme <artyrizcherei@gmail.com>
2021-01-13 19:54:51 +0300

    adf

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-// var a = +prompt("1", '');
-// var b = +prompt("2", '');
-// var c = a + b;
-// alert(c);
+// var check = "https://google.com.html"
+
+// console.log(check.startsWith("https")); 
+// console.log(check.endsWith(".html"))
+
+// function check(){
+//     var check1 = "https://google.com.html"
+//     if ((check1.startsWith ("https://")) && (check1.endsWith (".html"))){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(check())
+
+
+var a = "aajlkjjskdjfAlsj;gkejflksajfjskda";
+var b = {};
+for (let i = 0; i < a.length; i++){
+    if((a.match(new RegExp(a[i], "g"))).length > 1){
+        b[a[i]]=(a.match(new RegExp(a[i], "g"))).length;
+    }
+}
+console.log(b);
 
 
 
 
+<!DOCTYPE html>
+<html>
+<body>
 
-// let a = +prompt("1", '');
-// let b = +prompt("2", '');
-// let c = a + b;
-// alert(c);
+<p>Click the button to return the number of characters in the string "Hello World!".</p>
 
+<button onclick="myFunction()">Try it</button>
 
-function doSomething() {
-    let input = 'ssss';
-    input = prompt('Do something?');
-    if (input === null) {
-        return; //break out of the function early
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  var str = "Hello";
+  var n = str.length;
+  document.getElementById("demo").innerHTML = n;
+}
+</script>
+
+</body>
+</html>
+
